3e0171fe41ca4c4c4a4e78c787a47c76c14df588
jebarcha <jebarcha@hotmail.com>
2024-07-19 10:13:28 -0600

    fix to get count from ProductWithFiltersForCountSpecification

--- Core/Specifications/ProductWithFiltersForCountSpecification.cs
+++ Core/Specifications/ProductWithFiltersForCountSpecification.cs
@@ -11,7 +11,7 @@ public class ProductWithFiltersForCountSpecification : BaseSpecification<Product
 {
     public ProductWithFiltersForCountSpecification(ProductSpecParams productParams)
         : base(x =>
-            (!string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(
+            (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(
                 productParams.Search)) &&
             (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
             (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
